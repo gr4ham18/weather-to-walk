55530599246a3ad4793cfca2ddf8fe7b45d7f524
Connor Hollis <connorhollis3d@gmail.com>
2021-11-23 20:01:23 -0700

    added images for each dog but need to only have one dog img at a time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,46 @@
 var dogSectionEl = document.getElementById("dog-selection");
-console.log(dogSectionEl);
+var dogImgContainerEl = document.getElementById("dog-img-container");
+
 
 async function start() {
     const response = await fetch('https://api.thedogapi.com/v1/breeds')
     const data = await response.json()
-    //console.log(data[0].name)
+    console.log(data);
+    console.log(data[0].image);
     createDogNames(data);
+
 }
 
 start()
 
 
 function createDogNames(breedNames){
-    for(var i = 0; i < 1; i++){// change to breedNames.lenght afterwards
+    for(var i = 0; i < 2; i++){// change to breedNames.lenght afterwards
 
         //console.log(breedNames[i].name);
 
-
-
-
         //create dog list
-        var dogNameEl = document.createElement("option")
-
+        dogNameEl = document.createElement("option")
+        dogImageEl = document.createElement("img")
+ 
         //adds dog names to list
         dogNameEl.innerHTML = breedNames[i].name;
+        //dogImageEl.innerHTML = breedNames[i].image.url;
+
+        //give dog option an id
+        dogNameEl.id = "dog-" + i;
+        dogImageEl.src = breedNames[i].image.url;
 
         //append option to select elements in html
         dogSectionEl.appendChild(dogNameEl);
+        dogImgContainerEl.appendChild(dogImageEl);
 
     }
 
 }
+
+// function createDogImage(breedNames){
+
+//     if
+
+// }
