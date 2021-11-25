ec6b16851ef619b72aae5bf2fed8b102e30afb35
Connor Hollis <connorhollis3d@gmail.com>
2021-11-24 17:40:38 -0700

    added function to select specific img id to display dog img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var dogSectionEl = document.getElementById("dog-selection");
 var dogImgContainerEl = document.getElementById("dog-img-container");
+var dogImageEl = document.createElement("img");
+var dogNames = [];
 
 
 async function start() {
@@ -8,35 +10,63 @@ async function start() {
     console.log(data);
     console.log(data[0].image);
     createDogNames(data);
-
+    dogNames = data;
 }
 
-start()
+start();
 
 
 function createDogNames(breedNames){
-    for(var i = 0; i < 2; i++){// change to breedNames.lenght afterwards
+    for(var i = 0; i < breedNames.length; i++){// change to breedNames.lenght afterwards
 
+        
         //console.log(breedNames[i].name);
 
         //create dog list
-        dogNameEl = document.createElement("option")
-        dogImageEl = document.createElement("img")
- 
+        var dogNameEl = document.createElement("option");
+
         //adds dog names to list
         dogNameEl.innerHTML = breedNames[i].name;
         //dogImageEl.innerHTML = breedNames[i].image.url;
 
         //give dog option an id
         dogNameEl.id = "dog-" + i;
-        dogImageEl.src = breedNames[i].image.url;
+
+        //dogImageEl.src = breedNames[i].image.url;
+
+        //dogNameEl.setAttribute("value", breedNames[i].name);
 
         //append option to select elements in html
         dogSectionEl.appendChild(dogNameEl);
         dogImgContainerEl.appendChild(dogImageEl);
 
+
+
+
     }
 
+}
+
+
+
+
+
+
+dogSectionEl.addEventListener("change", function(){
+    var selectValue = getValue("dog-selection");
+    for(var i = 0; i < dogNames.length; i++){
+        if(selectValue === dogNames[i].name){    
+            dogImageEl.src = dogNames[i].image.url;
+            console.log(dogNames[i]);
+        }
+    }
+});
+
+
+
+
+function getValue(id){
+    return document.getElementById(id).value;
 }
 
 // function createDogImage(breedNames){
